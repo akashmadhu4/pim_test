6e1bec40419159142145df5679e5d6d723bda6eb
Ubuntu <ubuntu@ip-172-31-1-113.us-east-2.compute.internal>
2025-02-06 04:35:18 +0000

    init

---
+++ download_sdk_and_setup_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the directory name
+DIR_NAME="upmem-2025.1.0-Linux-x86_64"
+
+# Check if the directory does not exist
+if [ ! -d "$DIR_NAME" ]; then
+    # Download the file
+    wget http://sdk-releases.upmem.com/2025.1.0/ubuntu_22.04/upmem-2025.1.0-Linux-x86_64.tar.gz
+
+    # Extract the file
+    tar -xvzf upmem-2025.1.0-Linux-x86_64.tar.gz
+
+    # Remove the tar.gz file after extraction (optional)
+    rm upmem-2025.1.0-Linux-x86_64.tar.gz
+fi
+
+# Change to the directory
+cd ./$DIR_NAME
+
+# Source the environment script
+source ./upmem_env.sh
